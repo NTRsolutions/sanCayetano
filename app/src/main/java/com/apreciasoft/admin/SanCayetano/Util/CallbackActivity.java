f485a178ae117a4d7207551826eb0bc770f6f422
Admin <Admin>
2018-10-08 13:50:32 -0400

    - Add fabric - Add Firebase - Change name package

--- app/src/main/java/com/apreciasoft/admin/SanCayetano/Util/CallbackActivity.java
+++
@@ -1,9 +0,0 @@
-package com.apreciasoft.admin.SanCayetano.Util;
-
-/**
- * Created by Jorge on 6/2/18.
- */
-
-public interface CallbackActivity {
-        void doSomething();
-}
---
+++ app/src/main/java/com/apreciasoft/mobile/SanCayetano/Util/CallbackActivity.java
@@ -0,0 +1,9 @@
+package com.apreciasoft.mobile.SanCayetano.Util;
+
+/**
+ * Created by Jorge on 6/2/18.
+ */
+
+public interface CallbackActivity {
+        void doSomething();
+}
